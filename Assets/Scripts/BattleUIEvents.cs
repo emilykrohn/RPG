d917a93e3302f744db5bb7569e8b6ed57bbfa7b0
Emily Krohn <emilykrohn2@gmail.com>
2024-07-23 14:40:18 -0700

    Print Message on Attack Button Click

---
+++ Assets/Scripts/BattleUIEvents.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BattleUIEvents : MonoBehaviour
+{
+    UIDocument document;
+
+    Button button;
+
+    private void Awake()
+    {
+        document = GetComponent<UIDocument>();
+
+        button = document.rootVisualElement.Q("Attack") as Button;
+        button.RegisterCallback<ClickEvent>(OnClick);
+    }
+
+    void OnClick(ClickEvent evt)
+    {
+        Debug.Log("test");
+    }
+}
